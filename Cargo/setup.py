81be132038901a04d6d82c271cb67d975a4c120d
starodumovski <starodumovski@starodumovski>
2022-12-02 00:29:31 +0300

    _

--- Cargo/setup.py
+++ Cargo/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='cargo',
       version='0.1',
@@ -6,6 +6,5 @@ setup(name='cargo',
       url='https://github.com/starodumovski/NIC_cargo/tree/main/Cargo',
       author='',
       author_email='*@innopolis.university',
-      license='MIT',
-      packages=['cargo'],
-      zip_safe=True)
+      # license='MIT',
+      packages=find_packages())
